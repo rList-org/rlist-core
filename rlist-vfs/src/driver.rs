be9199e62ac4dc39b025204b0d62b5c3f6b4a173
haruki <meteo.haru@gmail.com>
2024-03-24 20:21:56 +0000

    save

--- rlist-vfs/src/driver.rs
+++ rlist-vfs/src/driver.rs
@@ -4,11 +4,37 @@ use crate::combinable_dir::CombinableDir;
 use crate::static_combinable::StaticCombinableFile;
 
 #[async_trait]
+/// # Main trait of the driver
+///
+/// The trait means that the driver can
+/// 1. load `Config` and get `State`
+/// 2. keep the `State`
+/// 3. get the VFS from the `State`
+///
+/// ## `Config`
+///
+/// The configuration of the driver. The `Config` will be read from the configuration file directly.
+///
+/// For example, the API key of the cloud storage can be the `Config`.
+///
+/// ## `State`
+///
+/// The **immutable** state of the driver. For example, the connection to the cloud storage.
+///
+/// Of course, you can just copy `Config` to `State` if you don't need a state.
 pub trait CloudDriver<Config, State>: GetVfs
     where Config: Clone + Send + Sync + for<'a> Deserialize<'a>
 {
     /// Create a new instance of the driver.
     fn new(state: State) -> Self;
+
+    /// The name of the driver. For example, `Google Drive`.
+    /// It is used to identify the driver from the configuration file.
+    /// So, it **must be unique**.
+    ///
+    /// You can pull request to [rlist-drivers](https://github.com/rList-org/rlist-drivers)
+    /// to add your driver into the list. There is a check in match forks to make sure the driver name is unique.
+    fn driver_name() -> &'static str;
 
     /// If `Config` is `State`, returns `Config` directly.
     async fn load_config(config: Config) -> State;
@@ -17,6 +43,9 @@ pub trait CloudDriver<Config, State>: GetVfs
 }
 
 #[async_trait]
+/// The trait means that the driver **instance** can get the VFS from state by calling `get_vfs` function.
+///
+/// You **must** implement this trait by calling `reload_vfs` then return the result.
 pub trait GetVfs: Send + Sync {
     /// You **must** implement this method by calling `reload_vfs` then return the result.
     /// ## Example:
--- rlist-vfs/src/lib.rs
+++ rlist-vfs/src/lib.rs
@@ -1,11 +1,27 @@
+/// # Files that has static download link
+/// There is a default implementation of `Combinable` for `StaticDownloadLinkFile`.
+///
+/// Dynamic download link is not supported yet.
 pub mod static_combinable;
-mod combinable;
+
+/// # `Combinable` trait means that `Vec<T>` can be combined to `T`
+pub mod combinable;
+
+/// # `CombinableDir` is a directory that can be combined
 pub mod combinable_dir;
 mod rcu;
+
+/// # traits that driver must implement
+///
+/// if you want to implement a new driver, you must implement these traits
+/// - [CloudDriver](driver::CloudDriver)
+/// - [GetVfs](driver::GetVfs)
 pub mod driver;
+
 mod without_link;
 mod wheel;
 
+/// The state of rList server
 pub use wheel::Wheel;
 
 /// Basic VFS (Virtual File System) traits
