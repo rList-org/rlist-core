68ee95b80d8ede8711528c0ff6c165ae2dbbc78f
haruki <meteo.haru@gmail.com>
2024-04-03 07:18:40 +0100

    combine macro

--- rlist-vfs/src/combinable.rs
+++ rlist-vfs/src/combinable.rs
@@ -3,3 +3,12 @@ pub trait Combinable
 {
     fn combine(from: Vec<Self>) -> Self;
 }
+
+#[macro_export]
+/// Combine items to one item, items must be the same type and implement `Combinable`
+macro_rules! combine {
+    ($($item:expr),*) => {{
+        let items = vec![$($item),*];
+        $crate::combinable::Combinable::combine(items)
+    }};
+}
